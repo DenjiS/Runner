bf206eb2b7fede4e0594a7fd296176e86ba4c94b
Daniel <Sychkov.Daniil@yandex.ru>
2023-08-27 22:30:33 +0300

    Update BackgroundPlanet.cs

--- Assets/Source/Scripts/UI/BackgroundPlanet.cs
+++ Assets/Source/Scripts/UI/BackgroundPlanet.cs
@@ -4,7 +4,6 @@ public class BackgroundPlanet : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.TryGetComponent(out Destroyer destroyer))
         {
             gameObject.SetActive(false);
