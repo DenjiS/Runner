cd3121f53567cdb6013ba0b3bc8618f59c916950
Daniel <Sychkov.Daniil@yandex.ru>
2023-08-27 14:42:31 +0300

    Spawner cooldown randomization

--- Assets/Source/Scripts/Common/Spawner.cs
+++ Assets/Source/Scripts/Common/Spawner.cs
@@ -4,17 +4,29 @@ public class Spawner : ObjectPool
 {
     [SerializeField] private GameObject[] _templates;
     [SerializeField] private float _maxSecondsBetweenSpawn;
-    [SerializeField] private float _minSecondsBetweenSpawn;
+    [SerializeField][Min(0)] private float _minSecondsBetweenSpawn;
 
     private SpawnPoint[] _spawnPoints;
     private float _secondsBetweenSpawn;
     private float _elapsedTime = 0;
 
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (_maxSecondsBetweenSpawn < _minSecondsBetweenSpawn)
+            _maxSecondsBetweenSpawn = _minSecondsBetweenSpawn;
+    }
+#endif
+
     private void Awake()
     {
         Initialize(_templates);
-
         _spawnPoints = GetComponentsInChildren<SpawnPoint>();
+    }
+
+    private void Start()
+    {
+        _secondsBetweenSpawn = Random.Range(_minSecondsBetweenSpawn, _maxSecondsBetweenSpawn);
     }
 
     private void Update()
@@ -25,6 +37,8 @@ public class Spawner : ObjectPool
         && TryGetRandomObject(out GameObject enemy))
         {
             _elapsedTime = 0;
+            _secondsBetweenSpawn = Random.Range(_minSecondsBetweenSpawn, _maxSecondsBetweenSpawn);
+            Debug.Log(gameObject.name + _secondsBetweenSpawn);
 
             int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
             SetEnemy(enemy, _spawnPoints[spawnPointNumber].transform.position);
